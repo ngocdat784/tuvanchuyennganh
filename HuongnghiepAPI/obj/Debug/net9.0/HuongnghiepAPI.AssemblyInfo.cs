fe2aa2ae100f13cc644ac5021a6ec7760574b1eb
ngocdat784 <ngocdattran315@gmail.com>
2026-04-02 10:05:16 +0700

    aa
    aa

--- HuongnghiepAPI/obj/Debug/net9.0/HuongnghiepAPI.AssemblyInfo.cs
+++ HuongnghiepAPI/obj/Debug/net9.0/HuongnghiepAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuongnghiepAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704ea5e7ea590b17ae46291403f282fc3cc542e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e6c489c0f6a2e7d6e0fc7a72fcdd47173816ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuongnghiepAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuongnghiepAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
